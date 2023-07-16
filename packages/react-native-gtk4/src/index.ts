858d5e7972f7bc2ca53bd321e021d3a81d52888e
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-07-16 16:00:28 +0100

    Output cjs

--- packages/react-native-gtk4/src/index.ts
+++ packages/react-native-gtk4/src/index.ts
@@ -1,22 +1,25 @@
 import gi from "@girs/node-gtk"
-import { Gtk } from "@girs/node-gtk-4.0"
-import { Gdk } from "@girs/node-gdk-4.0"
-import { Gio } from "@girs/node-gio-2.0"
-import { GLib } from "@girs/node-glib-2.0"
-import { GObject } from "@girs/node-gobject-2.0"
-import { Pango } from "@girs/node-pango-1.0"
-import Reconciler from "./reconciler.js"
+import Gtk from "@girs/node-gtk-4.0"
+import Gdk from "@girs/node-gdk-4.0"
+import Gio from "@girs/node-gio-2.0"
+import GLib from "@girs/node-glib-2.0"
+import GObject from "@girs/node-gobject-2.0"
+import Pango from "@girs/node-pango-1.0"
 
+import Reconciler from "./reconciler.js"
 import "./generated/jsx.js"
 import "./overrides.js"
 import { withApplicationContext } from "./components/ApplicationProvider.js"
 
 Gtk.init()
-gi.startLoop()
 
 let currentTag = 0
 
 export default function render(element: React.ReactNode, app: Gtk.Application) {
+  const loop = GLib.MainLoop.new(null, false)
+
+  gi.startLoop()
+
   app.on("activate", () => {
     const container = Reconciler.createContainer(
       app,
@@ -35,6 +38,8 @@ export default function render(element: React.ReactNode, app: Gtk.Application) {
       null,
       () => {}
     )
+
+    loop.run()
   })
 
   app.run([])

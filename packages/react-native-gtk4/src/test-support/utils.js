11580f5a05e290784efeaa4998503f9ae4ebfd16
Eugenio Depalo <eugeniodepalo@gmail.com>
2024-03-31 23:53:28 +0100

    Remove unused test support file

--- packages/react-native-gtk4/src/test-support/utils.js
+++
@@ -1,9 +0,0 @@
-import AbstractWidget from "../widget.js"
-import Gtk from "@girs/node-gtk-4.0"
-
-export function createMockWidget() {
-  return new (class extends AbstractWidget {
-    set() {}
-    commitMount() {}
-  })(new Gtk.Widget())
-}
